655e3a267837346ba50e95f28405669249f08b8e
airadmai <dashab0307@gmail.com>
2023-11-07 10:49:58 +0500

    Поиск повторяющихся элементов

--- lib.py
+++ lib.py
@@ -1,13 +1,12 @@
-def count_common_elements(*lists):
+def count_elements(lists):
     if len(lists) < 2:
         return 0
-
-    common_elements = set(lists[0])
+    elements = set(lists[0])
 
     for i in range(1, len(lists)):
-        common_elements = common_elements.intersection(set(lists[i]))
+        elements = elements.intersection(set(lists[i]))
 
-    return len(common_elements)
+    return len(elements)
 
 # Получение количества списков от пользователя
 n = int(input("Введите количество списков: "))
@@ -15,9 +14,9 @@ n = int(input("Введите количество списков: "))
 # Получение элементов каждого списка
 lists = []
 for i in range(n):
-    elements = input(f"Введите элементы {i + 1}-го списка через пробел: ").split()
-    lists.append(elements)
+      elements = input(f"Введите элементы {i + 1}-го списка через пробел: ").split()
+      lists.append(elements)
 
-# Вызов функции count_common_elements
-common_count = count_common_elements(*lists)
-print(f"Количество одинаковых элементов в списках: {common_count}")
+# # Вызов функции count_common_elements
+common_count = count_elements([[2, 5, 7, 7, 8, 2], [2, 5, 7, 10], [2, 7]])
+print("Количество одинаковых элементов в списках:", common_count)
