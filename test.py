81274c903da10fbe5731fbf241ed4773c9d29e34
Alena <Bartosh0409@yandex.ru>
2023-11-07 11:17:23 +0500

    test

---
+++ test.py
@@ -0,0 +1,8 @@
+import pytest
+import lib
+lists=[[2,5,7,7,8,2],[2,5,7,10],[2,7]]
+def test_CountElements_good():
+    assert lib.count_elements(lists)==2
+
+def test_CountElements_bad():
+    assert lib.count_elements(lists)==5
